644e2a5767d9c4bd84333facf7c31fc3539ec41a
jesusvaz <jesusvaz@gmail.com>
2024-01-24 14:02:29 -0700

    add hero iages

--- app/page.jsx
+++ app/page.jsx
@@ -1,43 +1,30 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import Blusas from '../public/blusas.jpeg'
+import Dobldillos from '../public/dobladillos.jpeg'
+import Zipper from '../public/zipper.jpeg'
+import Mangas from '../public/mangas.jpeg'
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Corte y Confeccion&nbsp;
-        </p> */}
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <Link
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Mending Sew{' '}
-            {/* <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            /> */}
-          </Link>
-        </div>
+    <main className="flex min-h-screen flex-col items-center  ">
+      <div class="grid grid-cols-2 gap-4 lg:grid-cols-4">
+        <Image src={Blusas} alt="Image 1" class="w-full h-64 object-cover rounded-lg shadow-lg" />
+        <Image src={Zipper} alt="Image 2" class="w-full h-64 object-cover rounded-lg shadow-lg" />
+        <Image src={Mangas} alt="Image 3" class="w-full h-64 object-cover rounded-lg shadow-lg" />
+        <Image src={Dobldillos} alt="Image 4" class="w-full h-64 object-cover rounded-lg shadow-lg" />
       </div>
-
+      <br/>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <div className="text-5xl">Marias' Textile Solutions</div>
       </div>
 
-      <p  className="text-2xl  ">Indulge in the transformative power of a perfectly fitted outfit recevtly acquire. Our curated collection ensures impeccable tailoring, enhancing your unique style and boosting confidence. Stand out in society with clothing that speaks volumes about your attention to detail and commitment to self-presentation.</p>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <Link
           href="/vastillas"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-       
+
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -54,7 +41,7 @@ export default function Home() {
         <Link
           href="/buttons"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-      
+
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -71,7 +58,7 @@ export default function Home() {
         <Link
           href="/zippers"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          
+
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -88,7 +75,7 @@ export default function Home() {
         <Link
           href="/sleeves"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          
+
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -105,7 +92,7 @@ export default function Home() {
         <Link
           href="/blusas"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          
+
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -122,7 +109,7 @@ export default function Home() {
         <Link
           href="/vestidos"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          
+
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -139,7 +126,7 @@ export default function Home() {
         <Link
           href="/dobladillos"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        
+
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -156,7 +143,7 @@ export default function Home() {
         <Link
           href="/ojales"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    
+
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -173,7 +160,7 @@ export default function Home() {
         <Link
           href="/pantalones"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-     
+
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -187,7 +174,7 @@ export default function Home() {
           </p>
         </Link>
 
-      
+
 
       </div>
 
