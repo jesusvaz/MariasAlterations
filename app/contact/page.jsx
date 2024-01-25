085b70b6677732ff245928ee0894f3e206eb5f02
jesusvaz <jesusvaz@gmail.com>
2024-01-24 17:11:28 -0700

    Change hero to altertions

---
+++ app/contact/page.jsx
@@ -0,0 +1,18 @@
+
+import Image from 'next/image'
+import foto from '../../public/marias.jpeg'
+export default function contact() {
+    return (
+        <main className="flex min-h-screen flex-col items-center  p-4">
+            Connect with us today! Reach us by phone at Marias' Textile Solutionsand 
+            <br/>801-691-6241<br/>
+            explore more on our social platforms: 
+            <br/>Facebook, 
+            <br/>Instagram, 
+            <br/>X. 
+            <br/>We look forward to engaging with you!"
+        </main>
+        
+    )
+}
+
