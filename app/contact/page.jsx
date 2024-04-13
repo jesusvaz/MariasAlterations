ebbfe5acb89494464d196ab47117e17a96fada5d
jesusvaz <jesusvaz@gmail.com>
2024-04-13 13:34:04 -0600

    add contact info

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -6,8 +6,11 @@ export default function contact({ params, searchParams }) {
     return (
         <main className="flex min-h-screen flex-col items-center  p-4">
 
-            {searchParams.lang === 'sp' ? <Link href='/'>Inicio</Link> : <Link href='/'>Home</Link>}
-       
+            {searchParams.lang === 'sp' ? <h3>Telefono</h3>: <h3>Phone</h3>} 801-691-6241
+            <br />
+            {searchParams.lang === 'sp' ? <h3>Correo electronico</h3>: <h3>Email</h3>} 
+            carrill_o@hotmail.com
+
             <div>
                 {searchParams.lang === 'sp' ? <Link href='/'>Inicio</Link> : <Link href='/'>Home</Link>}
             </div>
