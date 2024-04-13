ae9432dc9522c5b619c6f8a00df11480813dbfbc
jesusvaz <jesusvaz@gmail.com>
2024-04-13 09:50:00 -0600

    delete contactions

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -16,7 +16,7 @@ export default function about() {
             <br/>
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm l">
 
-                En Marias' Textile Solutions, nos dedicamos apasionadamente a transformar
+                En Marias Textile Solutions, nos dedicamos apasionadamente a transformar
                 la experiencia de vestir, brindando a nuestros clientes la confianza
                 y el atractivo que proviene de prendas impecablemente remendadas con acabados
                 profesionales. Nuestro objetivo es realzar la belleza individual de cada persona,
@@ -24,7 +24,7 @@ export default function about() {
                 con calidad y estilo. Con nuestra atención meticulosa a los detalles y artesanía experta,
                 te invitamos a descubrir cómo la perfección en cada puntada puede marcar la diferencia,
                 elevando tu presencia y dejando una impresión duradera en cada ocasión.
-                ¡Viste con confianza, viste con Marias' Textile Solutions!
+                ¡Viste con confianza, viste con Marias Textile Solutions!
             </div>
         </main>
         
